efaef75dfacc46e467b6286b5192403f638eec39
Youssef Abada <youssef.abada.x@gmail.com>
2020-01-05 00:57:33 +0100

    add responsive animated menu for mobile view

---
+++ Youssef/Code/script.js
@@ -0,0 +1,15 @@
+let toggle = false;
+function showMenu(){
+    let menuIcon = document.getElementById("menu-icon");
+    let menu = document.querySelector(".nav-menu .nav-links ul");
+    if (toggle){
+        menuIcon.innerHTML = '<i class="fa fa-bars" ></i>'
+        menu.style.right = "-82%";
+        toggle = !toggle;
+ 
+    } else {
+        menuIcon.innerHTML = '<i class="fa fa-times" ></i>'
+        menu.style.right = "0";
+        toggle = !toggle;
+    }     
+}
