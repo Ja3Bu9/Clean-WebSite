c2c86039b80977a9072487e3eb2ffcb5e562d322
Youssef Abada <youssef.abada.x@gmail.com>
2020-01-05 23:23:56 +0100

    add validation for contact form in contact page

--- Youssef/Code/script.js
+++ Youssef/Code/script.js
@@ -13,3 +13,30 @@ function showMenu(){
         toggle = !toggle;
     }     
 }
+
+function checkInput(type){
+    let input = document.getElementById(type);
+    if(type === 'name'){
+        // check for a valid full name contain at lease 3 characters
+        markError(/^([A-Za-z]{3,})+([\s[A-Za-z]?]?)+$/.test(input.value), input, type);
+    }else if(type === 'email'){
+        // check for a valid email
+        markError(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value), input,type);
+    }else if( type === 'subject'){
+        // subject must be 5 or more characters
+        markError(input.value.length >= 5, input, type);
+    }else if(type === 'message'){
+        // message must be 10 or more characters
+        markError(input.value.length >= 10, input, type);
+    }
+}
+
+function markError(state,input, type){
+        if(!state){
+            input.style.borderBottomColor = "Tomato";
+            document.getElementById("warning-"+type).style.display = "block";
+        }else{
+            input.style.borderBottomColor = "MediumSeaGreen";
+            document.getElementById("warning-"+type).style.display = "none";
+        }
+}
